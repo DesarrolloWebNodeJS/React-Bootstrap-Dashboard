592b5bdcb96413688df99c916450e96867b00735
Vladimir Rodríguez <fizzvr@gmail.com>
2018-12-10 16:05:24 -0500

    Ruta para la beta. > Desarrollador 🔋
    Desarrollo Web NodeJS • Quito Ecuador
    
    [Fix: ]

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,21 +1,20 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import NavBarView from './Visual/NavBarView';
-import FooterView from './Visual/FooterView';
+// NOTA: Las vistas podrian estar dentro del contenedor.
+// import NavBarView from './Visual/NavBarView';
+// import FooterView from './Visual/FooterView';
 import HomeView from './Visual/HomeView';
+import BienvenidoPagina from './contenedores/BienvenidoPagina';
 
 const AppRouter = () => {
   return (
     <Router>
-      <React.Fragment>
-        <NavBarView />
         <Switch>
           <Route path="/" exact component={HomeView} />
+          <Route path="/beta" exact component={BienvenidoPagina} />
           {/* <Route path="/about/" component={About} />
         <Route path="/users/" component={Users} /> */}
         </Switch>
-        <FooterView />
-      </React.Fragment>
     </Router>
   )
 }
